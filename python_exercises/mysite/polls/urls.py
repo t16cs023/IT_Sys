1804b2b4524643b83bc22bbf7a29eee108d788f6
t16cs023 <t16cs023@yamanashi.ac.jp>
2018-10-22 23:38:46 +0900

    [fix]polls/urls.pyを訂正
    pathをurlに変更

--- python_exercises/mysite/polls/urls.py
+++ python_exercises/mysite/polls/urls.py
@@ -4,7 +4,6 @@ Created on 2018/10/17
 @author: t16cs023
 '''
 
-from django.urls import path
 from django.conf.urls import url
 
 from . import views
@@ -12,8 +11,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-        path('', views.index, name='index'),
-        path('<int:question_id>/', views.detail, name='detail'),
-        path('<int:question_id>/results/', views.results, name='results'),
-        path('<int:question_id>/vote/', views.vote, name='vote'),
+        url('', views.index, name='index'),
+        url('<int:question_id>/', views.detail, name='detail'),
+        url('<int:question_id>/results/', views.results, name='results'),
+        url('<int:question_id>/vote/', views.vote, name='vote'),
     ]
