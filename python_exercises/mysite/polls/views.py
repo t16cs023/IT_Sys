5ba255035d60172a3d8bd6d0d9c1e1c396503aa5
t16cs023 <t16cs023@yamanashi.ac.jp>
2018-10-23 16:35:48 +0900

    [fix]投票後遷移しない不具合を修正

--- python_exercises/mysite/polls/views.py
+++ python_exercises/mysite/polls/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponseRedirect
+from django.http import  Http404
+from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
@@ -30,13 +32,26 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice"
-            })
+            'error_message': "You didn't select a choice.",
+        })
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
         
-        return HttpResponseRedirect(
-            reverse('polls:results', args=(question.id,))
-        )
-    
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+# 
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
+# 
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
